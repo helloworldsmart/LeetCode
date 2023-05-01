5c4a3b8fbb90e78d2223fe7bf4ab99712e768f49
MichaelChen <helloworldsmart3@gmail.com>
2023-05-01 16:30:33 +0800

    Time: 4 ms (78.00%), Space: 14.3 MB (26.00%) - LeetHub

---
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.swift
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func checkTree(_ root: TreeNode?) -> Bool {
+        guard let root = root, 
+              let leftVal = root.left?.val, 
+              let rightVal = root.right?.val 
+        else {
+          return false
+        }
+      return root.val == leftVal + rightVal
+    }
+}
