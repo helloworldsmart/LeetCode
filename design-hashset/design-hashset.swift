88e3eea7716972b4c9989bd05ba3e7f8851ccc2b
MichaelChen <helloworldsmart3@gmail.com>
2023-05-03 18:37:20 +0800

    Time: 287 ms, Memory: 15.5 MB - LeetHub

---
+++ design-hashset/design-hashset.swift
@@ -0,0 +1,54 @@
+
+// class MyHashSet {
+
+//     init() {
+        
+//     }
+    
+//     func add(_ key: Int) {
+        
+//     }
+    
+//     func remove(_ key: Int) {
+        
+//     }
+    
+//     func contains(_ key: Int) -> Bool {
+        
+//     }
+// }
+
+class MyHashSet {
+
+    private var set = [[Int]](repeating: [], count: 10000)
+    
+    private func hash(_ key: Int) -> Int {
+        return key % set.count
+    }
+    
+    func add(_ key: Int) {
+        if !contains(key) {
+            set[hash(key)].append(key)
+        }
+    }
+    
+    func remove(_ key: Int) {
+        if let index = set[hash(key)].firstIndex(of: key) {
+            set[hash(key)].remove(at: index)
+        }
+    }
+    
+    func contains(_ key: Int) -> Bool {
+        return set[hash(key)].contains(key)
+    }
+    
+}
+
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * let obj = MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * let ret_3: Bool = obj.contains(key)
+ */
