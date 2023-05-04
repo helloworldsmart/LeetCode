4613527a1cf2cb49922a0383a9ccb4fdd21e8b04
MichaelChen <helloworldsmart3@gmail.com>
2023-05-04 20:31:13 +0800

    Time: 26 ms (89.81%), Space: 14.7 MB (50.93%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func duplicateZeros(_ arr: inout [Int]) {
+        var newArr = [Int]()
+        for index in 0 ... arr.count - 1  {
+            if arr[index] == 0 {
+                // arr[index + 1] = 0
+                newArr.append(0)
+                newArr.append(0)
+            } else {
+                newArr.append(arr[index])
+            }
+        }
+        arr = Array(newArr.prefix(arr.count))
+    }
+}
