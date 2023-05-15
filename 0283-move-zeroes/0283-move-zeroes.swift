46d0a25d28bc27de2ea0a34e7ceca0097a16e93a
MichaelChen <helloworldsmart3@gmail.com>
2023-05-15 21:21:56 +0800

    Time: 170 ms (27.52%), Space: 15.1 MB (71.22%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func moveZeroes(_ nums: inout [Int]) {
+        var index = 0
+        for num in nums {
+            if num != 0 {
+                nums[index] = num
+                index += 1
+            }
+        }
+        
+        while index < nums.count {
+            nums[index] = 0
+            index += 1
+        }
+    }
+}
