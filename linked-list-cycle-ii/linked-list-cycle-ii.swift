502b0a69e6a3fa71d61f5fc99792e31289d3e940
MichaelChen <helloworldsmart3@gmail.com>
2023-05-18 20:56:57 +0800

    Time: 41 ms, Memory: 15.3 MB - LeetHub

---
+++ linked-list-cycle-ii/linked-list-cycle-ii.swift
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func detectCycle(_ head: ListNode?) -> ListNode? {
+        var slow = head
+        var fast = head
+        
+        while fast != nil && fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+            
+            if slow === fast {
+                break
+            }
+        }
+        
+        if fast == nil || fast?.next == nil {
+            return nil
+        }
+        
+        slow = head
+        while slow !== fast {
+            slow = slow?.next
+            fast = fast?.next
+        }
+        return slow
+    }
+}
