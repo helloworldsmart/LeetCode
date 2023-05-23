1b6f1855a8b05beabf5f5ca7462c09f94616e824
MichaelChen <helloworldsmart3@gmail.com>
2023-05-23 20:06:13 +0800

    Time: 386 ms, Memory: 15.4 MB - LeetHub

---
+++ design-hashmap/design-hashmap.swift
@@ -0,0 +1,44 @@
+
+class MyHashMap {
+    
+    private var buckets: [[(Int, Int)]]
+    private let bucketSize = 1000
+
+    init() {
+        buckets = Array(repeating: [], count: bucketSize)
+    }
+    
+    func put(_ key: Int, _ value: Int) {
+        let hashValue = key % bucketSize
+        for (index, kv) in buckets[hashValue].enumerated() {
+            if kv.0 == key {
+                buckets[hashValue][index] = (key, value)
+                return 
+            }
+        }
+        buckets[hashValue].append((key,value))
+    }
+    
+    func get(_ key: Int) -> Int {
+        let hashValue = key % bucketSize
+        for kv in buckets[hashValue] {
+            if kv.0 == key {
+                return kv.1
+            }
+        }
+        return -1
+    }
+    
+    func remove(_ key: Int) {
+        let hashValue = key % bucketSize
+        buckets[hashValue].removeAll(where: { $0.0 == key })
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * let obj = MyHashMap()
+ * obj.put(key, value)
+ * let ret_2: Int = obj.get(key)
+ * obj.remove(key)
+ */
