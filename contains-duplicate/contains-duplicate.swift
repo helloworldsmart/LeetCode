8d7f68afd579f09c8ff1467c27f7614385debfe6
MichaelChen <helloworldsmart3@gmail.com>
2023-08-04 00:12:51 +0800

    Time: 568 ms, Memory: 18.7 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        var distinctSet = Set<Int>()
+        for num in nums {
+           if distinctSet.contains(num) {
+               return true
+           } else {
+               distinctSet.insert(num)
+           }
+        }
+        return false
+    }
+}
